bc4e05e45b3eb97fd05411336edd6d4f84437392
sofiaversoza <sofversoza@gmail.com>
2022-09-08 21:16:29 -0700

    Set up Webpack

---
+++ src/index.js
@@ -0,0 +1,9 @@
+// MISC HELPERS
+function randomFromArray(array) {
+  return array[Math.floor(Math.random() * array.length)]
+}
+function getKeyString(x, y) {
+  return `${x}x${y}`
+}
+
+console.log('hi from index.js')
